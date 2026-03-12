de7f3d58904eddd4666e69b3217bc5d667b91ca7
James Daugherty <jdaugherty@jdresources.net>
2026-03-12 08:55:25 -0400

    test: add test coverage

--- grails-core/src/test/groovy/grails/boot/config/GrailsEnvironmentPostProcessorSpec.groovy
+++ grails-core/src/test/groovy/grails/boot/config/GrailsEnvironmentPostProcessorSpec.groovy
@@ -19,10 +19,14 @@
 package grails.boot.config
 
 import grails.util.Environment
+import org.apache.grails.core.plugins.GrailsPluginDescriptor
 import org.apache.grails.core.plugins.GrailsPluginDiscovery
+import org.apache.grails.core.plugins.GrailsPluginInfo
+import org.apache.grails.core.plugins.GrailsPluginLoadMetadata
 import org.springframework.boot.ConfigurableBootstrapContext
 import org.springframework.boot.SpringApplication
 import org.springframework.core.env.StandardEnvironment
+import org.springframework.core.io.Resource
 import spock.lang.Specification
 import spock.util.environment.RestoreSystemProperties
 
@@ -81,6 +85,139 @@ class GrailsEnvironmentPostProcessorSpec extends Specification {
 
         expect:
         processor.order < 0  // Should run early (HIGHEST_PRECEDENCE + 15 is still negative)
+    }
+
+    def "postProcessEnvironment handles IOException from yml configuration gracefully"() {
+        given:
+        def ymlResource = Mock(Resource)
+        ymlResource.exists() >> true
+        ymlResource.getFilename() >> 'plugin.yml'
+        ymlResource.getInputStream() >> { throw new IOException('Simulated yml read failure') }
+
+        def ymlPlugin = createPluginInfo('ymlPlugin', ymlResource)
+
+        def bootstrapContext = Mock(ConfigurableBootstrapContext)
+        def discovery = Mock(GrailsPluginDiscovery)
+        bootstrapContext.get(GrailsPluginDiscovery.class) >> discovery
+        discovery.getLoadOrderedPlugins(_) >> [ymlPlugin]
+
+        def processor = new GrailsEnvironmentPostProcessor(bootstrapContext)
+        def environment = new StandardEnvironment()
+        def application = Mock(SpringApplication)
+
+        when:
+        processor.postProcessEnvironment(environment, application)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "postProcessEnvironment handles IOException from groovy configuration gracefully"() {
+        given:
+        def groovyResource = Mock(Resource)
+        groovyResource.exists() >> true
+        groovyResource.getFilename() >> 'plugin.groovy'
+        groovyResource.getURL() >> { throw new IOException('Simulated groovy read failure') }
+
+        def groovyPlugin = createPluginInfo('groovyPlugin', groovyResource)
+
+        def bootstrapContext = Mock(ConfigurableBootstrapContext)
+        def discovery = Mock(GrailsPluginDiscovery)
+        bootstrapContext.get(GrailsPluginDiscovery.class) >> discovery
+        discovery.getLoadOrderedPlugins(_) >> [groovyPlugin]
+
+        def processor = new GrailsEnvironmentPostProcessor(bootstrapContext)
+        def environment = new StandardEnvironment()
+        def application = Mock(SpringApplication)
+
+        when:
+        processor.postProcessEnvironment(environment, application)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "postProcessEnvironment handles both yml and groovy configuration failures gracefully"() {
+        given:
+        def ymlResource = Mock(Resource)
+        ymlResource.exists() >> true
+        ymlResource.getFilename() >> 'plugin.yml'
+        ymlResource.getInputStream() >> { throw new IOException('Simulated yml read failure') }
+
+        def groovyResource = Mock(Resource)
+        groovyResource.exists() >> true
+        groovyResource.getFilename() >> 'plugin.groovy'
+        groovyResource.getURL() >> { throw new IOException('Simulated groovy read failure') }
+
+        def ymlPlugin = createPluginInfo('ymlPlugin', ymlResource)
+        def groovyPlugin = createPluginInfo('groovyPlugin', groovyResource)
+
+        def bootstrapContext = Mock(ConfigurableBootstrapContext)
+        def discovery = Mock(GrailsPluginDiscovery)
+        bootstrapContext.get(GrailsPluginDiscovery.class) >> discovery
+        discovery.getLoadOrderedPlugins(_) >> [ymlPlugin, groovyPlugin]
+
+        def processor = new GrailsEnvironmentPostProcessor(bootstrapContext)
+        def environment = new StandardEnvironment()
+        def application = Mock(SpringApplication)
+
+        when:
+        processor.postProcessEnvironment(environment, application)
+
+        then:
+        noExceptionThrown()
+    }
+
+    def "postProcessEnvironment does not add property sources when both yml and groovy configurations fail"() {
+        given:
+        def ymlResource = Mock(Resource)
+        ymlResource.exists() >> true
+        ymlResource.getFilename() >> 'plugin.yml'
+        ymlResource.getInputStream() >> { throw new IOException('Simulated yml read failure') }
+
+        def groovyResource = Mock(Resource)
+        groovyResource.exists() >> true
+        groovyResource.getFilename() >> 'plugin.groovy'
+        groovyResource.getURL() >> { throw new IOException('Simulated groovy read failure') }
+
+        def ymlPlugin = createPluginInfo('ymlPlugin', ymlResource)
+        def groovyPlugin = createPluginInfo('groovyPlugin', groovyResource)
+
+        def bootstrapContext = Mock(ConfigurableBootstrapContext)
+        def discovery = Mock(GrailsPluginDiscovery)
+        bootstrapContext.get(GrailsPluginDiscovery.class) >> discovery
+        discovery.getLoadOrderedPlugins(_) >> [ymlPlugin, groovyPlugin]
+
+        def processor = new GrailsEnvironmentPostProcessor(bootstrapContext)
+        def environment = new StandardEnvironment()
+        def application = Mock(SpringApplication)
+        def initialSourceCount = environment.propertySources.size()
+
+        when:
+        processor.postProcessEnvironment(environment, application)
+
+        then:
+        noExceptionThrown()
+        environment.propertySources.size() == initialSourceCount
+    }
+
+    private static GrailsPluginInfo createPluginInfo(String name, Resource configResource) {
+        def metadata = new GrailsPluginLoadMetadata(
+                name,
+                '1.0',
+                '*',
+                CoreTestGrailsPlugin,
+                new String[0],
+                new String[0],
+                Collections.emptyMap(),
+                new String[0],
+                new String[0],
+                new String[0],
+                Collections.emptyMap(),
+                true
+        )
+        def descriptor = new GrailsPluginDescriptor(null, [], [])
+        return new GrailsPluginInfo(descriptor, metadata, configResource, false)
     }
 }
 
